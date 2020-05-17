aa1b7832eb2fbfbfde09aeca82f44374ed76fcb0
Anirudh Kanukanti <anirudhkanukanti@gmail.com>
2020-05-17 13:01:15 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   percent = 50;
+  radius = 20;
 }
--- src/app/progress-circle/progress-circle.component.ts
+++ src/app/progress-circle/progress-circle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-progress-circle',
@@ -15,15 +15,22 @@ export class ProgressCircleComponent implements OnChanges {
 
   constructor() { }
 
-  ngOnChanges(): void {
-    const circumference = this.radius * 2 * 22 / 7;
-    const progressPercent = Math.min(100, (this.completed / this.total) * 100);
-    const offset = circumference * (1 - progressPercent / 100);
-    this.circleStyles = {
-      'stroke-dasharray': circumference,
-      'stroke-dashoffset': offset,
-      'stroke-width': this.radius * 2
-    };
+  ngOnChanges(changes: SimpleChanges): void {
+    if ((changes.radius && changes.radius.currentValue !== changes.radius.previousValue)
+      || (changes.completed && changes.completed.currentValue !== changes.completed.previousValue)
+      || (changes.total && changes.total.currentValue !== changes.total.previousValue)
+    ) {
+      const circumference = 2 * Math.PI * this.radius;
+      const progressPercent = Math.min(100, (this.completed / this.total) * 100);
+      // const offset = circumference * (1 - progressPercent / 100);
+      const offset = circumference * (progressPercent / 100);
+      this.circleStyles = {
+        // 'stroke-dasharray': circumference,
+        // 'stroke-dashoffset': offset,
+        'stroke-dasharray': offset + ' ' + circumference,
+        'stroke-width': this.radius * 2
+      };
+    }
   }
 
 }
